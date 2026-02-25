022de01374654af7a0c92f5519b3f10a2b4116c2
Nam Viet <buinamviet15705@gmail.com>
2026-02-25 12:15:49 +0700

    update

--- src/crawl/product_crawler.py
+++ src/crawl/product_crawler.py
@@ -681,7 +681,7 @@ def get_current_price_only(driver, url, product_id):
 
 def update_price_offer(cur, conn, driver):
     try:
-        cur.execute("SELECT product_id, product_url FROM product ORDER BY product_id ASC LIMIT 85000;")
+        cur.execute("SELECT product_id, product_url FROM product ORDER BY product_id ASC;")
         products = cur.fetchall()
         print(f"Starting to update price for {len(products)} products...")
         count = 0
